ef6721832e8e9315caa9791e0983eeb0a3997688
新风作浪 <duxinfeng99@gmail.com>
2013-05-03 18:11:31 +0800

    Initial Commit

---
+++ SearchPerson33/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SearchPerson33
+//
+//  Created by DolBy on 13-5-3.
+//  Copyright (c) 2013年 Duxinfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
